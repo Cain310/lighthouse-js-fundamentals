3feeeec53ca6d8ab2d2f3328157bc52199a99f94
Michael Cain <michaelcaincb@gmail.com>
2019-02-22 16:38:14 +0000

    Ingredients list forwards and back exercise

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+// Write a for loop that prints out the contents of ingredients:
+for (var i1 = 0; i1 < ingredients.length; i1++) {
+  console.log(ingredients[i1]);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+var reversed = ingredients.reverse(i2);
+for (var i2 = 0; i2 < ingredients.length; i2++) {
+  console.log(ingredients[i2]);
+}
